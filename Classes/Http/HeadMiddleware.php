30997b1229e6b2dda3a7b39861a1fbfaa74a28c7
kluge <kilian.kluge@3m5.de>
2026-08-21 10:18:10 +0200

    Initial commit

---
+++ Classes/Http/HeadMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DMF\SitePackage\Http;
+
+use Neos\Flow\Annotations as Flow;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
+
+class HeadMiddleware implements MiddlewareInterface
+{
+    /**
+     * @Flow\Inject
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // TODO: Implement process() method.
+        $response = $handler->handle($request);
+        $this->logger->info("Head Middleware Response : " . $response->getBody());
+        return $response;
+    }
+}
