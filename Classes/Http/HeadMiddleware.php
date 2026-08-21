ecb089062da0c76e2ac8fac3f46f3c02901ac9d5
kluge <kilian.kluge@3m5.de>
2026-08-21 11:27:07 +0200

    Initial commit

--- Classes/Http/HeadMiddleware.php
+++ Classes/Http/HeadMiddleware.php
@@ -4,6 +4,8 @@ namespace DMF\Capo\Http;
 
 use DMF\Capo\Capo\Parser;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\ContentStream;
+use Neos\Flow\Log\Utility\LogEnvironment;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -13,6 +15,11 @@ use Psr\Log\LoggerInterface;
 class HeadMiddleware implements MiddlewareInterface
 {
     /**
+     * @Flow\InjectConfiguration(package="Capo", path="options")
+     */
+    protected array $options = [];
+
+    /**
      * @Flow\Inject
      * @var LoggerInterface
      */
@@ -20,12 +27,33 @@ class HeadMiddleware implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        // TODO: Implement process() method.
+        $usedOptions = $this->options ?? [];
+
         $response = $handler->handle($request);
         $body = $response->getBody();
-        $this->logger->info("Head Middleware Response : " . $body);
-        $body = Parser::reorder_head($body);
-        $this->logger->info("Head Middleware Response : " . $body);
-        return $response->withBody($body);
+
+        $body = Parser::reorder_head(strval($body), $usedOptions);
+
+        $analysis = Parser::get_last_analysis();
+        if ($usedOptions['debug']) {
+            $this->logger->debug('Capo: element_count'.$analysis['element_count'].', elapsed:'.$analysis['elapsed_ms'].', warnings'.count($analysis['warnings']).', tokens:'.count($analysis['tokens']));
+        }
+        if ($usedOptions['display_warnings']) {
+            $warnings = $analysis['warnings'];
+            $this->logger->warning('Warnings: '.count($warnings));
+            foreach ($warnings as $warning) {
+                $this->logger->warning($warning);
+            }
+        }
+        if ($usedOptions['display_weights']) {
+            $tokens = $analysis['tokens'];
+            $this->logger->debug('Tokens: '.count($tokens));
+            foreach ($tokens as $token) {
+                $this->logger->debug($token['tag_name'].': '.$token['weight']);
+            }
+        }
+
+        $stream = ContentStream::fromContents($body);
+        return $response->withBody($stream);
     }
 }
